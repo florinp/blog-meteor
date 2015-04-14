aa402e72ce07c09689d1a5241ae8b411f07b3f62
Florin Pavel <florinp94@gmail.com>
2015-04-14 12:55:17 +0300

    up

--- server/smtp.js
+++ server/smtp.js
@@ -1,8 +1,8 @@
 Meteor.startup(function(){
     smtp = {
         host: 'smtp.gmail.com',
-        username: 'florin.pavel@east-wolf.com',
-        password: 'ben100ale',
+        username: '',
+        password: '',
         port: 465
     };
     process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.host) + ':' + encodeURIComponent(smtp.port);
